4641329a9d8745dfd34b038aca55ca53a1d64b2d
Manas verma <manasverma1357@gmail.com>
2025-04-12 19:58:04 +0530

    fix bugs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function add(a, b){
     return a+b;
 }
 function subtract(a,b){
-    return a-b;
+    return a-b;   
 }
 function multiply(a,b){
     return a*b;
@@ -12,7 +12,3 @@ function divide(a,b){
 }
 const a = parseInt(prompt("Enter your first number:"))
 const b = parseInt(prompt("Enter your second number: "))
-console.log(add(a,b))
-console.log(subtract(a,b))
-console.log(multiply(a,b))
-console.log(divide(a,b))
